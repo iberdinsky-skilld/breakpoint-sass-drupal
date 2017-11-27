a1a8fef031e49d644094d9b66d921c2d0ad5723f
iberdinsky <finnsky2010@gmail.com>
2017-11-27 18:09:16 +0600

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var YAML = require('yamljs')
 var fs = require('fs')
 var stream = require('stream')
 var through = require('through2')
+var mediaQuery = require('css-mediaquery')
 
 var breakpointSassFromDrupal = {}
 
@@ -13,18 +14,80 @@ function jsonToScssMap (obj, mapName) {
     var breakpoint = obj[i]
     var multipliers = getMultipliers(breakpoint)
     var label = ( mapName ? mapName + '--' : '' ) + breakpoint.label
+    var query = queryToBreakpointSass(mediaQuery.parse(breakpoint.mediaQuery));
+
     if (multipliers) {
       for (var j in multipliers) {
-        scssMap += '  ' + label + '__' + multipliers[j] + ': \'' + breakpoint.mediaQuery + '\',\n'
+        scssMap += '  ' + label + '__' + multipliers[j] + ': \'' + query + '\',\n'
       }
     } else {
-      scssMap += '  ' + label + ': \'' + breakpoint.mediaQuery + '\',\n'
+      scssMap += '  ' + label + ': \'' + query + '\',\n'
     }
   }
 
   scssMap += ');'
 
   return scssMap
+}
+
+function queryToBreakpointSass(query) {
+  var output = ''
+  for (var i in query) {
+    var part = query[i]
+    var pairWidth = ['', '']
+    var pairHeight = ['', '']
+    output += (part.inverse ? 'not ' : '') + part.type
+
+    for (var j in part.expressions) {
+      var expression = part.expressions[j]
+      var modifier = expression.modifier ? expression.modifier + '-' : ''
+      var value = expression.value ? ' ' + expression.value : ''
+
+      if (expression.feature == 'width') {
+
+        if (expression.modifier == 'min') {
+          pairWidth[0] = expression.value
+        } else if (expression.modifier == 'max') {
+          pairWidth[1] = expression.value
+        }
+        if (j == part.expressions.length - 1) {
+          if (pairWidth[0] == '') {
+            output += pairWidth[1] ? '(max-width ' + pairWidth[1] + ')' : ''
+          } else {
+            output += ' (' +  pairWidth.join(' ') + ')'
+          }
+        }
+
+      } else if (expression.feature == 'height') {
+
+        if (expression.modifier == 'min') {
+          pairHeight[0] = expression.value
+        } else if (expression.modifier == 'max') {
+          pairHeight[1] = expression.value
+        }
+        if (j == part.expressions.length - 1) {
+
+          if (pairHeight[0] == '') {
+            output += pairHeight[1] ? '(max-height ' + pairHeight[1] + ')' : ''
+          } else {
+            output += ' (height ' +  pairHeight.join(' ') + ')'
+          }
+          console.log(output);
+        }
+
+
+      } else {
+
+        output += ' (' + modifier + expression.feature + value + ')'
+
+      }
+    }
+
+    if (i < query.length - 1) {
+      output += ', '
+    }
+  }
+  return output
 }
 
 /**
